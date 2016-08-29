c099eaa6601047b1d1674745417536fa7b826f3b
Tobi <scholl.s.tobias@web.de>
2016-08-29 11:04:08 +0200

    erstellung php-files + header/footer
    Erstellung der php-files
    Erstellung des Grundgerüsts für header + footer

---
+++ php_files/account_select.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>API-Abfrage</title>
+    <link rel="shortcut icon" href="../res/img/favicon.ico">
+    <link rel="stylesheet" href="../res/css/main.css" charset="utf-8">
+  </head>
+  <body>
+    <header>
+      <h3>Account-Auswahl</h3>
+      <nav>
+        <ul>
+          <a href="../index.html" class="active"><li>Startseite</li></a>
+          <a class="active"><li>Account-Auswahl</li></a>
+          <a href="php_files/char_overview.php"><li>Charakter-Übersicht</li></a>
+          <a href="php_files/char_compare.php"><li>Charakter-Vergleich</li></a>
+        </ul>
+      </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+      <p>©1996 - 2016 Blizzard Entertainment, Inc. All rights reserved.<br>Battle.net, Blizzard Entertainment, Diablo and Reaper of Souls are trademarks or registered trademarks of Blizzard Entertainment, Inc. in the U.S. and/or other countries.</p>
+    </footer>
+  </body>
+</html>
---
+++ php_files/variables.php
@@ -0,0 +1,6 @@
+<?php
+
+$apikey = "27hd5khrafctn6dv8g9egtbt25qbzcqz";
+
+
+?>
